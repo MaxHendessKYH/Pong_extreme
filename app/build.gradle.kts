4ec71902a44a1642b75b431af226074408f4f682
mvp <meh.tavakoli@gmail.com>
2023-11-28 10:53:17 +0100

    changed activity_main.xml and activity_playing_classic.xml also add end game button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.pong_extreme"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.pong_extreme"
@@ -15,6 +15,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures{
+        viewBinding = true
     }
 
     buildTypes {
--- app/src/main/java/com/example/pong_extreme/PlayingClassicActivity.kt
+++ app/src/main/java/com/example/pong_extreme/PlayingClassicActivity.kt
@@ -1,11 +1,21 @@
 package com.example.pong_extreme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pong_extreme.databinding.ActivityPlayingClassicBinding
 
 class PlayingClassicActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityPlayingClassicBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_playing_classic)
+
+        binding = ActivityPlayingClassicBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnEndGame.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+        }
     }
 }
