348c8ec00925e790a749c4eb02884ee4231979e8
Dennis <nilssondennis97@gmail.com>
2023-11-28 09:33:59 +0100

    Skeleton

--- app/src/main/java/com/example/pong_extreme/PlayingClassicActivity.kt
+++ app/src/main/java/com/example/pong_extreme/PlayingClassicActivity.kt
@@ -3,11 +3,9 @@ package com.example.pong_extreme
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
-
-class MainActivity : AppCompatActivity() {
+class PlayingClassicActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_playing_classic)
     }
 }
