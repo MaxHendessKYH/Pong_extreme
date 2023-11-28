4209de952569afde45fcffbcd0d8c7ae787a4dbb
mvp <meh.tavakoli@gmail.com>
2023-11-28 11:13:11 +0100

    start game buttons functionality added

--- app/src/main/java/com/example/pong_extreme/MainActivity.kt
+++ app/src/main/java/com/example/pong_extreme/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.example.pong_extreme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pong_extreme.databinding.ActivityMainBinding
 
 
 
 class MainActivity : AppCompatActivity() {
+
+ lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnClassic.setOnClickListener {
+            val intent = Intent(this, PlayingClassicActivity::class.java)
+        }
+
+        binding.btnTimed.setOnClickListener {
+            val intent = Intent(this, PlayingTimedActivity::class.java)
+        }
+
+
     }
 }
