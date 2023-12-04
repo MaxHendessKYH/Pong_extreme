f8c2ce162dc2ddd9a3e150a06d77acccae513edc
Juhee Kang <juhee.kang@infotiv.se>
2023-12-04 14:57:06 +0100

    set bounds

--- app/src/main/java/com/example/pong_extreme/Ball.kt
+++ app/src/main/java/com/example/pong_extreme/Ball.kt
@@ -3,6 +3,7 @@ package com.example.pong_extreme
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Rect
 
 class Ball(context: Context, var color: Int, var posX: Float, var posY: Float, var size: Float, var speedX: Float, var speedY: Float) {
 
@@ -17,5 +18,16 @@ class Ball(context: Context, var color: Int, var posX: Float, var posY: Float, v
     {
         posX += speedX
         posY += speedY
+    }
+
+    fun checkBounds(bounds: Rect){
+        if(posX-size < bounds.left || posX+size > bounds.right){
+            speedX *= -1
+            posX += speedX*1.2f
+        }
+        if(posY-size < bounds.top || posY+size > bounds.bottom){
+            speedY *= -1
+            posY += speedY*1.2f
+        }
     }
 }
