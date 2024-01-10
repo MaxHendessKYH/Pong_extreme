d8b1b17cb9bf58d12919fafed78ccdb08968e290
Mehdi <meh.tavakoli@gmail.com>
2024-01-10 17:14:02 +0100

    Five powerup icons added. size: 40*40 pixel. done with bitmap!

--- app/src/main/java/com/example/pong_extreme/PowerupManager.kt
+++ app/src/main/java/com/example/pong_extreme/PowerupManager.kt
@@ -1,14 +1,49 @@
 package com.example.pong_extreme
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import kotlin.random.Random
 
-class PowerupManager {
-        enum class PowerUpType(val bitmap: Int) {
-            BIGPADDLE(R.drawable.bigpaddle),
-            SMALLPADDLE(R.drawable.smallpaddle),
-            STICKY(R.drawable.sticky),
-            SLOWMOTION(R.drawable.slowmotion),
-            MULTIBALLS(R.drawable.multiballs)
+
+    class PowerupManager (context: Context, type: PowerUpType) {
+
+        var bitmap: Bitmap
+        enum class PowerUpType {
+            BIGPADDLE,
+            SMALLPADDLE,
+            STICKY,
+            SLOWMOTION,
+            MULTIBALLS
         }
+
+        init {
+            bitmap = when (type) {
+                PowerUpType.BIGPADDLE ->
+                    BitmapFactory.decodeResource(context.resources, R.drawable.bigpaddle)
+                PowerUpType.SMALLPADDLE ->
+                    BitmapFactory.decodeResource(context.resources, R.drawable.smallpaddle)
+                PowerUpType.STICKY ->
+                    BitmapFactory.decodeResource(context.resources, R.drawable.sticky)
+                PowerUpType.SLOWMOTION ->
+                    BitmapFactory.decodeResource(context.resources, R.drawable.slowmotion)
+                else ->
+                    BitmapFactory.decodeResource(context.resources, R.drawable.smallpaddle)
+            }
+        }
+
+//        init {
+//            bitmap = if (type == PowerUpType.BIGPADDLE) {
+//                BitmapFactory.decodeResource(context.resources, R.drawable.bigpaddle)
+//            } else if (type == PowerUpType.SMALLPADDLE){
+//                BitmapFactory.decodeResource(context.resources, R.drawable.smallpaddle)
+//            }else if (type == PowerUpType.STICKY){
+//                BitmapFactory.decodeResource(context.resources, R.drawable.sticky)
+//            }else if (type == PowerUpType.SLOWMOTION){
+//                BitmapFactory.decodeResource(context.resources, R.drawable.slowmotion)
+//            }else {
+//                BitmapFactory.decodeResource(context.resources, R.drawable.smallpaddle)
+//            }
+//        }
 
     var activePower = "None"
     var powerupActive: Boolean = false
@@ -32,4 +67,5 @@ class PowerupManager {
                 stickyTimer = 30
             }
     }
+
 }
